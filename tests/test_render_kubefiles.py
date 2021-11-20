1bd45aa7b07f1c7261673b0104b99ebdb3c609b3
Stéphane Bidoul <stephane.bidoul@gmail.com>
2021-11-20 15:06:34 +0100

    Fix addons directory location
    It was mixed with the odoo data dir.

--- tests/test_render_kubefiles.py
+++ tests/test_render_kubefiles.py
@@ -34,7 +34,7 @@ configMapGenerator:
   - name: odooenv
     literals:
       - PGDATABASE=build-name
-      - ADDONS_DIR=/mnt/data/odoo-data-dir
+      - ADDONS_DIR=/mnt/data/odoo-addons-dir
       - RUNBOAT_GIT_REPO=https://github.com/oca/mis-builder
       - RUNBOAT_GIT_REF=abcdef123456789
   - name: runboat-scripts
