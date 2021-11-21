a090c6160e1c31b4a382eff8375c7dd668f91cf7
Stéphane Bidoul <stephane.bidoul@gmail.com>
2021-11-21 19:09:20 +0100

    Add tests for github webhook

---
+++ tests/test_webhook.py
@@ -0,0 +1,68 @@
+import pytest
+from fastapi.testclient import TestClient
+from pytest_mock import MockerFixture
+
+from runboat.app import app
+from runboat.controller import controller
+from runboat.github import CommitInfo
+
+client = TestClient(app)
+
+
+def test_webhook_github_push(mocker: MockerFixture) -> None:
+    mock = mocker.patch("fastapi.BackgroundTasks.add_task")
+    response = client.post(
+        "/webhooks/github",
+        headers={
+            "X-GitHub-Event": "push",
+        },
+        json={
+            "repository": {"full_name": "oca/mis-builder"},
+            "ref": "refs/heads/15.0",
+            "after": "abcde",
+        },
+    )
+    response.raise_for_status()
+    mock.assert_called_with(
+        controller.deploy_or_start,
+        CommitInfo(
+            repo="oca/mis-builder",
+            target_branch="15.0",
+            pr=None,
+            git_commit="abcde",
+        ),
+    )
+
+
+@pytest.mark.parametrize("action", ["opened", "synchronize"])
+def test_webhook_github_pr_open_synchronize(action: str, mocker: MockerFixture) -> None:
+    mock = mocker.patch("fastapi.BackgroundTasks.add_task")
+    response = client.post(
+        "/webhooks/github",
+        headers={
+            "X-GitHub-Event": "pull_request",
+        },
+        json={
+            "action": action,
+            "repository": {"full_name": "oca/mis-builder"},
+            "pull_request": {
+                "base": {
+                    "ref": "15.0",
+                },
+                "number": 381,
+                "head": {
+                    "sha": "abcde",
+                },
+            },
+        },
+    )
+    response.raise_for_status()
+    mock.assert_called_with(
+        controller.deploy_or_start,
+        CommitInfo(
+            repo="oca/mis-builder",
+            target_branch="15.0",
+            pr=381,
+            git_commit="abcde",
+        ),
+    )
