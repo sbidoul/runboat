8b31beed55d587fae594e7db11ee60957e8cbdec
Stéphane Bidoul <stephane.bidoul@gmail.com>
2021-11-24 19:32:35 +0100

    --db-filter is necessary for Odoo 10 too, it seems

--- src/runboat/kubefiles/runboat-start.sh
+++ src/runboat/kubefiles/runboat-start.sh
@@ -11,10 +11,11 @@ bash /runboat/runboat-clone-and-install.sh
 oca_wait_for_postgres
 
 
-# --db_user is necessary for Odoo <= 10
+# --db_user and --db_filter is necessary for Odoo <= 10
 
 unbuffer $(which odoo || which openerp-server) \
   --data-dir=/mnt/data/odoo-data-dir \
   --no-database-list \
   -d ${PGDATABASE} \
-  --db_user ${PGUSER}
+  --db-filter=^${PGDATABASE}
+  --db_user=${PGUSER}
