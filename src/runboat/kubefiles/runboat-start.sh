9e1a21f40920ec29a18bc42fb4dad76d0a00daf1
Stéphane Bidoul <stephane.bidoul@gmail.com>
2021-11-24 19:05:01 +0100

    Fix startup script for Odoo<=10

--- src/runboat/kubefiles/runboat-start.sh
+++ src/runboat/kubefiles/runboat-start.sh
@@ -10,7 +10,11 @@ bash /runboat/runboat-clone-and-install.sh
 
 oca_wait_for_postgres
 
+
+# --db_user is necessary for Odoo <= 10
+
 unbuffer $(which odoo || which openerp-server) \
   --data-dir=/mnt/data/odoo-data-dir \
   --no-database-list \
-  -d ${PGDATABASE}
+  -d ${PGDATABASE} \
+  --db_user ${PGUSER}
