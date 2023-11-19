bc534e923c9d65490448b6a1073051af6558c678
Stéphane Bidoul <stephane.bidoul@gmail.com>
2023-11-19 12:52:32 +0100

    Resolve fastapi deprecation warning

--- src/runboat/app.py
+++ src/runboat/app.py
@@ -1,23 +1,27 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from . import __version__, api, controller, k8s, webhooks, webui
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    await k8s.load_kube_config()
+    await controller.controller.start()
+    yield
+    await controller.controller.stop()
+
+
 app = FastAPI(
-    title="Runboat", description="Runbot on Kubernetes ☸️", version=__version__
+    title="Runboat",
+    description="Runbot on Kubernetes ☸️",
+    version=__version__,
+    lifespan=lifespan,
 )
 app.include_router(api.router, prefix="/api/v1", tags=["api"])
 app.include_router(webhooks.router, tags=["webhooks"])
 app.include_router(webui.router, tags=["webui"])
 
 webui.mount(app)
-
-
-@app.on_event("startup")
-async def startup() -> None:
-    await k8s.load_kube_config()
-    await controller.controller.start()
-
-
-@app.on_event("shutdown")
-async def shutdown() -> None:
-    await controller.controller.stop()
